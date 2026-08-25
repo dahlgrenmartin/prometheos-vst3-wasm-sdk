43260b3c7a6ab0fa6afdbecce9e66daa7b9883a4
Martin Dahlgren <mdahlgrengadd@users.noreply.github.com>
2026-08-25 14:02:01 +0200

    fix: stage VST3 teardown and isolate stereo process buffers

--- src/adapter/vst3_instance.h
+++ src/adapter/vst3_instance.h
@@ -15,7 +15,7 @@ public:
   ~Vst3Instance (); Vst3Instance (const Vst3Instance&) = delete;
   int32_t process (const float* input, float* output, uint32_t frames); int32_t reset ();
   int32_t note (bool on, int32_t note, float velocity); int32_t set_parameter (uint32_t id, float value); float get_parameter (uint32_t id) const;
-  bool save_state (std::vector<uint8_t>& result); bool load_state (const uint8_t* data, uint32_t size);
+  bool save_state (std::vector<uint8_t>& result); int32_t load_state (const uint8_t* data, uint32_t size);
   int32_t parameter_count () const; bool parameter_info (uint32_t index, Steinberg::Vst::ParameterInfo& info) const; bool parameter_value_text (uint32_t id, float value, Steinberg::Vst::String128 text) const;
 private:
   class HostApplication final : public Steinberg::Vst::IHostApplication {
@@ -27,6 +27,6 @@ private:
   Vst3Instance () = default; bool start (const Steinberg::TUID class_id, double sample_rate);
   void release (); static bool success (Steinberg::tresult result) { return result == Steinberg::kResultOk; }
   HostApplication host_; Steinberg::Vst::IComponent* component_ {nullptr}; Steinberg::Vst::IAudioProcessor* processor_ {nullptr}; Steinberg::Vst::IEditController* controller_ {nullptr}; Steinberg::Vst::IConnectionPoint* component_connection_ {nullptr}; Steinberg::Vst::IConnectionPoint* controller_connection_ {nullptr};
-  std::array<float, 128> silence_ {}; std::array<Steinberg::Vst::Sample32*, 2> input_channels_ {}; std::array<Steinberg::Vst::Sample32*, 2> output_channels_ {}; std::array<Steinberg::Vst::AudioBusBuffers, 1> input_buses_ {}; std::array<Steinberg::Vst::AudioBusBuffers, 1> output_buses_ {}; FixedEventList events_; FixedParameterChanges parameters_; uint32_t max_frames_ {0}; bool has_input_ {false};
+  std::array<float, 128> silence_ {}; std::array<float, 128> input_left_ {}, input_right_ {}, output_left_ {}, output_right_ {}; std::array<Steinberg::Vst::Sample32*, 2> input_channels_ {}; std::array<Steinberg::Vst::Sample32*, 2> output_channels_ {}; std::array<Steinberg::Vst::AudioBusBuffers, 1> input_buses_ {}; std::array<Steinberg::Vst::AudioBusBuffers, 1> output_buses_ {}; FixedEventList events_; FixedParameterChanges parameters_; uint32_t max_frames_ {0}; bool has_input_ {false}, input_active_ {false}, output_active_ {false}, component_active_ {false}, processing_ {false}, connected_component_ {false}, connected_controller_ {false};
 };
 } // namespace pvst
--- tests/native/adapter_process_test.cpp
+++ tests/native/adapter_process_test.cpp
@@ -5,11 +5,14 @@
 #undef assert
 #define assert(condition) do { if (!(condition)) std::abort (); } while (false)
 int process_test () {
-  std::array<float, 128> output {};
+  std::array<float, 256> output {};
   const auto synth = pvst_create (0, 48000.0, 128); assert (synth != 0);
   assert (pvst_note_on (synth, 60, .5f) == PVST_OK);
   assert (pvst_process (synth, nullptr, output.data (), 128) == PVST_OK);
   bool nonzero = false; for (auto value : output) nonzero |= value != 0.f; assert (nonzero);
   assert (pvst_process (synth, nullptr, output.data (), 129) == PVST_ERROR_FRAME_COUNT);
+  for (int i=0;i<64;++i) assert(pvst_note_on(synth,60,.5f)==PVST_OK); assert(pvst_note_on(synth,60,.5f)==PVST_ERROR_QUEUE_FULL); assert(pvst_process(synth,nullptr,output.data(),128)==PVST_OK);
+  std::array<float, 256> input {}; std::array<float, 256> effected {}; for (uint32_t i=0;i<128;++i) { input[2*i]=.25f; input[2*i+1]=.5f; }
+  const auto effect = pvst_create (1, 48000., 128); assert (effect != 0); assert (pvst_process(effect,input.data(),effected.data(),128)==PVST_OK); assert(effected[0] == .25f*.25f && effected[1] == .5f*.25f); pvst_destroy(effect);
   pvst_destroy (synth); return 0;
 }
